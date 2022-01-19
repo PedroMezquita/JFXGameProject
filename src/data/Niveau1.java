e7dcbbd84e02288f4fb0a0a0d139256328ed1af6
Pedro MEZQUITA FERNANDEZ <mezquita.pedro@protonmail.com>
2022-01-19 20:24:55 +0100

    Changement d'image du perso

--- src/data/Niveau1.java
+++ src/data/Niveau1.java
@@ -15,19 +15,19 @@ public class Niveau1 extends Niveau{
         Map map = new TestMap();
         Props obstacle1 = new Props(500,10,0,0,"Mur haut");
         obstacle1.setRGB(0,0,0);
-        obstacle1.setSprite("TemplateWall.jpg");
+        obstacle1.setSprite("TemplateWall.png");
         map.addEntity(obstacle1);
         Props obstacle2 = new Props(10,500,490,0,"Mur droite");
         obstacle2.setRGB(0,0,0);
-        obstacle2.setSprite("TemplateWall.jpg");
+        obstacle2.setSprite("TemplateWall.png");
         map.addEntity(obstacle2);
         Props obstacle3 = new Props(500,10,0,490,"Mur bas");
         obstacle3.setRGB(0,0,0);
-        obstacle3.setSprite("TemplateWall.jpg");
+        obstacle3.setSprite("TemplateWall.png");
         map.addEntity(obstacle3);
         Props obstacle4 = new Props(10,500,0,0,"Mur gache");
         obstacle4.setRGB(0,0,0);
-        obstacle4.setSprite("TemplateWall.jpg");
+        obstacle4.setSprite("TemplateWall.png");
         map.addEntity(obstacle4);
         Personnage joueur = new Joueur(1,1,10,10,10,10,5,"mec", /* 21*/ 41, /* 21*/41,20,20,"joueur", 1,0,0); //Le joueur charge avant les props
         joueur.setSprite("TemplateCharacter.png");
